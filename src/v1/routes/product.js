46f912e3acf6d2d48492cd0ff67993e1f2e6660c
jhoancanchila <canchila.jhoan@gmail.com>
2024-06-27 18:54:44 -0500

    add middleware validate role and authorization in route product

--- src/v1/routes/product.js
+++ src/v1/routes/product.js
@@ -1,19 +1,21 @@
 import express from 'express';
 import { createOneProduct, getAllProducts, getOneProduct, updateOneProduct, deleteOneProduct } from '../../controllers/productController.js';
+import { authenticateToken } from '../../auth/middleware/auth.js';
+import { authorizeRole } from '../../auth/middleware/authorize.js';
 
 export default function productRouter(app) {
   const router = express.Router();
   app.use('/api/v1', router);
   router
-    .get('/products', ( req, res ) => getAllProducts( req, res ))
+    .get('/products', authenticateToken, authorizeRole('admin'), ( req, res ) => getAllProducts( req, res ))
 
-    .get('/products/:id', ( req, res ) => getOneProduct( req, res ))  
+    .get('/products/:id', authenticateToken, authorizeRole('admin'), ( req, res ) => getOneProduct( req, res ))  
 
-    .post('/products', ( req, res ) => createOneProduct( req, res ))  
+    .post('/products', authenticateToken, authorizeRole('admin'), ( req, res ) => createOneProduct( req, res ))  
 
-    .put('/products/:id', ( req, res ) => updateOneProduct( req, res ))  
+    .put('/products/:id', authenticateToken, authorizeRole('admin'), ( req, res ) => updateOneProduct( req, res ))  
 
-    .delete('/products/:id', ( req, res ) => deleteOneProduct( req, res ));  
+    .delete('/products/:id', authenticateToken, authorizeRole('admin'), ( req, res ) => deleteOneProduct( req, res ));  
 
   return router;
 };
