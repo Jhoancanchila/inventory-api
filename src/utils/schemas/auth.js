9f5455f8906b0183806cd4b9b9c28d613bf7f879
jhoancanchila <canchila.jhoan@gmail.com>
2024-06-27 19:56:47 -0500

    add schema register in auth

---
+++ src/utils/schemas/auth.js
@@ -0,0 +1,14 @@
+import z from 'zod';
+
+const registerSchema = z.object({
+  email: z.string().email(
+    'Please provide a valid email address'
+  ),
+  password: z.string().min(6),
+  name: z.string(),
+  role: z.enum(['admin', 'client'])
+});
+
+export const validateRegister = (user) => {
+  return registerSchema.safeParse(user);
+}
